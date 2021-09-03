8a1eb56e15a9107513d51d4d99ae28bc669e5ef0
jitendhra <er.jithuro@gmail.com>
2021-09-03 20:48:26 +0400

    MessageBoxWindow Alignment Done

--- MB_Window.xaml.cs
+++ MB_Window.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -34,6 +35,26 @@ namespace MessageBox
             {
 
             }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        DoubleAnimation anim;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Closing -= Window_Closing;
+            e.Cancel = true;
+            anim = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(0.3));
+            anim.Completed += (s, _) => this.Close();
+            this.BeginAnimation(UIElement.OpacityProperty, anim);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Height = (txtText.LineCount *27) + Gbar.Height + 50;
         }
     }
 }
