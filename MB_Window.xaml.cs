401c572448c81f1773718da183b50e59744b9dbb
jitendhra <er.jithuro@gmail.com>
2021-09-03 18:54:02 +0400

    Add project files.

---
+++ MB_Window.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MessageBox
+{
+    /// <summary>
+    /// Interaction logic for Window_MB.xaml
+    /// </summary>
+    public partial class MB_Window : Window
+    {
+        public MB_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Gbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
